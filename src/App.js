88388698d55628b9732e0755d4a5422e2c6ca4dd
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-09-17 22:48:59 +0530

    Optimizing with useMemo()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
 import './App.css';
+import Checkout from './Checkout';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Home/>
+      <Router>
+        <Switch>
+          <Route path='/checkout'>
+            <Header/>
+            <Checkout/>
+          </Route>
+          <Route path='/'>
+            <Header/>
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
+     
+    
     </div>
   );
 }
