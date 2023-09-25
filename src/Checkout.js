7e0e869c9b90c08d058d3b2a70200da461a25ce0
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-09-25 20:24:50 +0530

    Add To Basket done

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,8 +2,11 @@ import React from 'react'
 import CheckoutProduct from './CheckoutProduct'
 import './Checkout.css'
 import Subtotal from './Subtotal'
+import { useStateValue } from './StateProvider'
 
 const Checkout = () => {
+
+  const [{basket}, dispatch] = useStateValue();
   return (
     <div className='checkout'>
       <div className='checkout__left'>
@@ -11,9 +14,9 @@ const Checkout = () => {
         <div className='checkout__title'>
           <h2>Your Shopping Basket</h2>
         </div>
-        <CheckoutProduct/>
-        <CheckoutProduct/>
-        <CheckoutProduct/>
+        {basket.map(item => (
+          <CheckoutProduct id={item.id} title={item.title} image={item.image} price={item.price} rating={item.rating}/>
+        ))}
       </div>
       <div className='checkout__right'>
         <Subtotal/>
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import './CheckoutProduct.css'
 
-const CheckoutProduct = () => {
+const CheckoutProduct = ({id,image, title, price, rating}) => {
   return (
     
          <div className='checkoutProduct'>
             
-            <img className='checkoutProduct__image' src='https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg'/>
+            <img className='checkoutProduct__image' src={image}/>
             <div className='checkoutProduct__info'>
                 <p className='checkoutProduct__title'>
-                Bennett Mystic Men Laptop Bag 15.6 inch Shoulder Briefcase Messenger Business Bags for Men & Women
-                </p>
+               {title}       </p>
                 <p className='checkoutProduct__price'>
                     <small>$</small>
-                    <strong>25</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className='checkoutProduct__rating'>
-                ⭐⭐⭐⭐
+                {Array(rating)
+                  .fill().map((i)=><p>⭐</p>)}
                 </div>
                 <button>Remove from Basket</button>
             </div>
