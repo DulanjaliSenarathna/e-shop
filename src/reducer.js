fa3b7dab9441c89aa186a7b7407f7f7afed37d80
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-09-25 22:05:54 +0530

    Subtotal update done

--- src/reducer.js
+++ src/reducer.js
@@ -5,6 +5,11 @@ export const initialState = {
 }
 
 //selector
+
+export const getBasketTotal = (basket) =>{
+    return(basket?.reduce((amount,item) => item.price + amount, 0));
+}
+
 const reducer = (state,action) =>{
     switch(action.type){
         case 'ADD_TO_BASKET':
